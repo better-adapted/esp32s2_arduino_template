7d6ae412b1bf71a9d9066545cfd2ecf567fbaea4
AW_RYZEN9 <adam_t_watson@hotmail.com>
2025-08-13 19:25:19 +0100

    first commit of ccd demo

--- main/template_main.cpp
+++ main/template_main.cpp
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 #include <Arduino.h>
+#include <WiFi.h>
+
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
@@ -15,14 +17,40 @@
 
 #define GPIO_NEOPIXEL 38
 
+#define GPIO_CCD_VID 1
+#define GPIO_CCD_EOS 2
+#define GPIO_CCD_TRIG 42
+#define GPIO_CCD_ST 41
+#define GPIO_CCD_CLK 40
+
+
 #ifdef GPIO_NEOPIXEL
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel pixels(1, GPIO_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 #endif
 
+void setup_io() {
+	// all SPI pins to SDCARD are idle inputs pulled high on boot or sleep
+	pinMode(GPIO_CCD_VID, INPUT);
+	pinMode(GPIO_CCD_EOS, INPUT);
+	pinMode(GPIO_CCD_TRIG, INPUT);
+
+	pinMode(GPIO_CCD_ST, OUTPUT);
+	digitalWrite(GPIO_CCD_ST, HIGH);
+	
+	pinMode(GPIO_CCD_CLK, OUTPUT);
+	digitalWrite(GPIO_CCD_CLK, HIGH);	
+}
+
 void setup(void)
 {
 	Serial.begin(115200);
+
+	Serial.println();
+	Serial.println("VBSS Demo starting");
+	Serial.println(String(__DATE__) + " " + String(__TIME__));
+
+	delay(10);	
 	
 #ifdef GPIO_NEOPIXEL
 	pixels.setPixelColor(0, pixels.Color(250, 0, 0)); // setup()
@@ -37,54 +65,13 @@ void setup(void)
 	pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // setup()
 	pixels.show();									// setup()
 	delay(200);
-#endif	
-	
-    Serial.println();
-	Serial.println("Hello world!");
-    Serial.println();
+#endif
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    Serial.printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-    Serial.println();
+	WiFi.mode(WIFI_OFF); // Disable WiFi
+	btStop();			 // Disable Bluetooth
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    Serial.printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    Serial.println();
-        
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        Serial.printf("Get flash size failed");
-    	Serial.println();        
-        return;
-    }
-
-    Serial.printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-    Serial.println();           
-
-    Serial.printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-       Serial.println();
-
-    for (int i = 10; i >= 0; i--) {
-        Serial.printf("Restarting in %d seconds...\n", i);
-    	Serial.println();        
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    
-    Serial.printf("Restarting now.\n");
-    Serial.println();
-    
-    fflush(stdout);
-    esp_restart();
+	setup_io();
+			
 }
 
 void loop()
