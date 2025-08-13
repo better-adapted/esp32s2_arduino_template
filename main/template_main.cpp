ebebbe4e31f81139eb887561ea545b1977a3ec85
AW_RYZEN9 <adam_t_watson@hotmail.com>
2025-08-13 18:44:06 +0100

    Arduino added as a submodule - Commit: de184bd0cbb6f8d6c68497236657a1cb591f5ce4 [de184bd] Parents: 1014937965 Author: Lewis He <lewishe@outlook.com> Date: 28 March 2025 16:38:31 Committer: GitHub Commit Date: 28 March 2025 10:38:31
    Building with ESP-IDF 5.4.1 !

--- main/template_main.cpp
+++ main/template_main.cpp
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: CC0-1.0
  */
-
+#include <Arduino.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
@@ -13,40 +13,57 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
-void app_main(void)
+void setup(void)
 {
-    printf("Hello world!\n");
+	Serial.begin(115200);
+	
+    Serial.printf("Hello world!\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    Serial.printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
            CONFIG_IDF_TARGET,
            chip_info.cores,
            (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
            (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
            (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
            (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    Serial.println();
 
     unsigned major_rev = chip_info.revision / 100;
     unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    Serial.printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    Serial.println();
+        
     if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
+        Serial.printf("Get flash size failed");
+    	Serial.println();        
         return;
     }
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    Serial.printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    Serial.println();           
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    Serial.printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+       Serial.println();
 
     for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
+        Serial.printf("Restarting in %d seconds...\n", i);
+    	Serial.println();        
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
+    
+    Serial.printf("Restarting now.\n");
+    Serial.println();
+    
     fflush(stdout);
     esp_restart();
+}
+
+void loop()
+{
+	
 }
